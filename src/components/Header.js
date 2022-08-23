1ab34500f88df05c43a0b77f44c7e235048b9e55
Ayesiza Hawah <yesiha2001@gmail.com>
2022-08-24 00:16:56 +0300

    chage images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
+
 
 // import link
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import logo
-import Logo from '../assets/img/logo.svg';
+//import Logo from "../assets/img/logo.svg";
+import {GiSpookyHouse}from 'react-icons/gi'
 
 const Header = () => {
   return (
-    <header className='py-6 mb-12 border-b'>
-      <div className='container mx-auto flex justify-between items-center'>
-        <Link to='/'>
-          <img src={Logo} alt='' />
+    <header className="py-6 mb-12 border-b">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/">
+          <GiSpookyHouse size={"50px"} className=" text-blue-700"/>
         </Link>
-        <div className='flex items-center gap-6'>
-          <Link className='hover:text-violet-900 transition' to='/'>
+        <div className="flex items-center gap-6">
+          <Link className="hover:text-blue-900 transition" to="/">
             Log in
           </Link>
           <Link
-            className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition'
-            to='/'
+            className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-2 rounded-lg transition"
+            to="/"
           >
             Sign up
           </Link>
